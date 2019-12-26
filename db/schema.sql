73b7d32bde6ae4066f0bc37d472632c07e08eac7
Hoo Man <hoomanfor@gmail.com>
2019-12-26 13:26:59 -0500

    fixed typo within Eaten table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,20 @@
 DROP DATABASE IF EXISTS healthy_db;
 CREATE DATABASE healthy_db; 
+
+-- Heroku Deployment Tables
+CREATE TABLE IF NOT EXISTS pkw7mw6qasxc1m7a.Moment (
+    `id` INTEGER NOT NULL auto_increment , 
+    `moment` VARCHAR(255), 
+    `createdAt` DATETIME NOT NULL, 
+    `updatedAt` DATETIME NOT NULL, 
+    PRIMARY KEY (`id`)) 
+    ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS pkw7mw6qasxc1m7a.Eat (
+    `id` INTEGER NOT NULL auto_increment , 
+    `name` VARCHAR(255) NOT NULL, `consumed` TINYINT(1) DEFAULT false, 
+    `createdAt` DATETIME NOT NULL, `updatedAt` 
+    DATETIME NOT NULL, `MomentId` INTEGER, 
+    PRIMARY KEY (`id`), 
+    FOREIGN KEY (`MomentId`) REFERENCES `Moment` (`id`) ON DELETE SET NULL ON UPDATE CASCADE) 
+    ENGINE=InnoDB;
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,20 @@ INSERT INTO healthy_db.moment (moment, createdAt, updatedAt) VALUES ("Morning", 
 INSERT INTO healthy_db.moment (moment, createdAt, updatedAt) VALUES ("Noon", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO healthy_db.moment (moment, createdAt, updatedAt) VALUES ("Afternoon", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO healthy_db.moment (moment, createdAt, updatedAt) VALUES ("Evening", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+-- Heroku Deployment Seeds
+INSERT INTO pkw7mw6qasxc1m7a.Moment (moment, createdAt, updatedAt) VALUES ("Morning", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Moment (moment, createdAt, updatedAt) VALUES ("Noon", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Moment (moment, createdAt, updatedAt) VALUES ("Afternoon", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Moment (moment, createdAt, updatedAt) VALUES ("Evening", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Edamame", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Cantaloupe", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Pomegranate", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, createdAt, updatedAt) VALUES ("Avocado", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Cucumber", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, createdAt, updatedAt) VALUES ("Jerky", CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Blackberries", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Blueberries", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Cashews", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO pkw7mw6qasxc1m7a.Eat (name, consumed, createdAt, updatedAt) VALUES ("Pear", 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
